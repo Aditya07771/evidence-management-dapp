fcfea6639452cc37a669956dd78e4916759de3ea
Aditya07771 <adityanishad753@gmail.com>
2026-04-14 21:12:18 +0530

    update hardhat config settings

---
+++ Blockchain/hardhat.config.ts
@@ -0,0 +1,58 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: false,
+    },
+    sepolia: {
+      url: process.env.RPC_URL || "https://sepolia.infura.io/v3/",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./src/contracts/artifacts",
+  },
+
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+    currency: "USD",
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
